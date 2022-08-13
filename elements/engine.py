ab1c422816a562f2d0b8650e03e8f44a1c4746bf
redfoxgun@gmail.com <fiyg@yandex.ru>
2022-08-13 14:09:53 +0300

    Add a score panel, score counting, game_over check, box value arrangement, selected box checker

--- elements/engine.py
+++ elements/engine.py
@@ -12,6 +12,7 @@ class GameCycle():
         self.level = 1
         self.difficulty = 2  # 4 - easy, 3 - normal, 2 - hard
         self.score = 0
+        self.selected_box = None
         self.run()
 
     def run(self):
@@ -45,11 +46,25 @@ class GameLogic():
             if (box.coord[0] < mouse_pos[0] < box.coord[0] + box.width and
                     box.coord[1] < mouse_pos[1] < box.coord[1] + box.height):
                 box.color = (0, 100, 100)
+                self.game_cycle.selected_box = box
 
     def box_click_up(self):
         for box in self.game_field.elements:
             box.color = (255, 255, 255)
-        self.next_level()
+
+        selected_box = self.game_cycle.selected_box
+        if selected_box is None:
+            return
+
+        # check if mouse position in clicked box while click up.
+        mouse_pos = pygame.mouse.get_pos()
+        if (selected_box.coord[0] < mouse_pos[0] < selected_box.coord[0] + selected_box.width and
+                selected_box.coord[1] < mouse_pos[1] < selected_box.coord[1] + selected_box.height):
+            self.add_score()
+            if not self.is_game_over():
+                self.next_level()
+            else:
+                self.new_game()
 
     def bad_box_arrangement(self):
         amount_boxes = len(self.game_field.elements)
@@ -63,16 +78,37 @@ class GameLogic():
                 is_bad = self.game_field.elements[random_box_number].is_bad
             self.game_field.elements[random_box_number].is_bad = True
 
+    def box_value_arrangement(self):
+        for box in self.game_field.elements:
+            if box.is_bad:
+                box.value = self.game_cycle.level * self.game_cycle.difficulty * -1
+            else:
+                box.value = self.game_cycle.level * self.game_cycle.difficulty
+
     def reset_boxes(self):
         for box in self.game_field.elements:
             box.is_bad = False
-         
+
     def next_level(self):
         self.game_cycle.level += 1
         self.reset_boxes()
         self.create_boxes()
         self.bad_box_arrangement()
+        self.box_value_arrangement()
 
     def new_game(self):
         self.game_cycle.level = 1
         self.reset_boxes()
+        self.game_cycle.score = 0
+        self.game_field.elements.clear()
+        self.create_boxes()
+
+    def add_score(self):
+        self.game_cycle.score += self.game_cycle.selected_box.value
+
+    def is_game_over(self) -> bool:
+        print(self.game_cycle.score)
+        if self.game_cycle.score < 0:
+            return True
+        else:
+            return False
--- elements/primitives.py
+++ elements/primitives.py
@@ -12,19 +12,22 @@ class Box():
         self.border_color = (0, 0, 0)
         self.is_bad = False
         self.value = 1
+        self.font = pygame.font.SysFont(None, 96)
         self.screen = screen
 
     def draw(self):
-        if self.is_bad :
-            self.color = (255, 0, 0)
+        color = self.color
+        # if self.is_bad :
+        #     color = (255, 0, 0)
         pygame.draw.rect(surface=self.screen,
-                         color=self.color,
+                         color=color,
                          border_radius=10,
                          rect=pygame.Rect(self.coord[0],
                                           self.coord[1],
                                           self.width,
                                           self.height))
         self.__draw_border()
+        self.__draw_value()
 
     def __draw_border(self):
         pygame.draw.rect(surface=self.screen,
@@ -36,6 +39,15 @@ class Box():
                                           self.width,
                                           self.height))
 
+    def __draw_value(self):
+        if self.is_bad:
+            color = (255, 0, 0)
+        else:
+            color = (0, 255, 0)
+        value_surface = self.font.render(str(self.value), True, color)
+        resized_value_surface = pygame.transform.scale(value_surface, (self.width, self.height))
+        self.screen.blit(resized_value_surface, (self.coord[0], self.coord[1]))
+
     def set_coord(self, x, y):
         self.coord = (x, y)
 
@@ -44,26 +56,50 @@ class Box():
         self.height = height
 
 
+class ScorePanel():
+    def __init__(self, screen: pygame.Surface, game_field):
+        self.screen = screen
+        self.game_field = game_field
+        self.color = (100, 100, 100)
+        self.coord = (0, 0)
+
+    def draw(self):
+        color = self.color
+        pygame.draw.rect(surface=self.screen,
+                         color=color,
+                         rect=pygame.Rect(self.coord[0],
+                                          self.coord[1],
+                                          self.screen.get_width(),
+                                          self.game_field.top_offset))
+        self.__draw_border()
+
+    def __draw_border(self):
+        pass
+
+
 class GameField():
     def __init__(self, size: tuple):
         self.elements = []
         self.size = size
         self.screen = pygame.display.set_mode(self.size)
+        self.top_offset = self.size[1] // 10
+        self.score_panel = ScorePanel(screen=self.screen, game_field=self)
 
     def __draw_elements(self):
         self.__arrange_elements()
         for element in self.elements:
             element.draw()
+        self.score_panel.draw()
 
     def __arrange_elements(self):
         field_width = self.size[0]
-        field_height = self.size[1]
+        field_height = self.size[1] - self.top_offset
         amount_boxes = len(self.elements)
         rows = cols = math.sqrt(amount_boxes)
         box_width = field_width / rows
         box_height = field_height / cols
         box_x = 0
-        box_y = 0
+        box_y = 0 + self.top_offset
         for n, elem in enumerate(self.elements):
             if n % cols == 0 and n > 0:
                 box_x = 0
@@ -73,7 +109,7 @@ class GameField():
             box_x += box_width
 
     def draw(self):
-        self.screen.fill((0, 0, 0))
+        self.screen.fill((100, 100, 100))
         self.__draw_elements()
 
     def add_box(self, coord=(0, 0)):
